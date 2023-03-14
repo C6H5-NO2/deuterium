c62ae7a78176d0127b3f0ef748fdaa25eee71a14
C6H5-NO2 <19368807+C6H5-NO2@users.noreply.github.com>
2023-03-14 17:39:20 +0100

    compose desktop gradle template

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "Deuterium"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,1 @@
+fun main() = println("Hello, 世界")
