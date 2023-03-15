375a3df94f06faf9b0a2db66d936f7af0f505f00
C6H5-NO2 <19368807+C6H5-NO2@users.noreply.github.com>
2023-03-15 20:36:45 +0100

    codeviewer 0b429b6

---
+++ src/main/kotlin/org/jetbrains/codeviewer/platform/File.kt
@@ -0,0 +1,15 @@
+package org.jetbrains.codeviewer.platform
+
+import kotlinx.coroutines.CoroutineScope
+import org.jetbrains.codeviewer.util.TextLines
+
+val HomeFolder: File get() = java.io.File(System.getProperty("user.home")).toProjectFile()
+
+interface File {
+    val name: String
+    val isDirectory: Boolean
+    val children: List<File>
+    val hasChildren: Boolean
+
+    fun readLines(scope: CoroutineScope): TextLines
+}
